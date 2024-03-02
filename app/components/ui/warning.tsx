18678aa2e3786a4907f28062f7729c683f79e0c3
Mostafa Waleeed <mostafawaleed206@proton.me>
2024-03-02 14:02:54 +0200

    Add absolute positioning and z-index to Warning component

--- app/components/ui/warning.tsx
+++ app/components/ui/warning.tsx
@@ -1,6 +1,6 @@
 export default function Warning() {
   return (
-    <div className="w-full h-10 bg-red-600 flex">
+    <div className="w-full h-10 bg-red-600 flex absolute z-50">
       <p className="text-white text-base text-center m-auto">
         Experimental app: Files auto-delete after 24 hours. Proceed with caution and backup accordingly.
       </p>
