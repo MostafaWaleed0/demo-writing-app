b2bcbc2e2bc0f49e56eed4d9e9c2bf011089e58f
Mostafa Waleeed <mostafawaleed206@proton.me>
2024-03-05 10:14:28 +0200

    Remove `min-h-full` class for center loading in page

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import { Loading } from '@/components/ui';
 
 export default function PageLoading() {
   return (
-    <div className="m-auto min-h-full">
+    <div className="m-auto">
       <Loading />
     </div>
   );
